9fa34fe3e5f59441ac7611744e8b85140c91f418
vigimella <giova4698@gmail.com>
2021-02-17 21:19:49 +0100

    Clean repository

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 """
 Author: Giovanni Ciaramella
+
 Date: February 14th 2021
-Description: gets apk file in input, extracts from it dex file and converts all in image in grayscale.
+
+Description: This tool gets an APK file in input, extracts from it dex file and converts it both in image in grayscale
+and color image. The APK files, after the upload,  will be stored into the APK_STORAGE with hash code to avoid duplicate
+applications with a different name.
+
+IF YOU USE THIS TOOL PLEASE QUOTE ME :)
 
 """
 
-
-import os, zipfile, math, codecs, shutil
+import os, zipfile, math, codecs, shutil, hashlib
 
 from flask import Flask, render_template, request
 from shutil import copyfile
@@ -15,6 +20,7 @@ from PIL import Image
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+
 @app.route('/')
 def hello_world():
     return render_template('index.html')
@@ -22,28 +28,59 @@ def hello_world():
 
 @app.route("/upload-apk", methods=['POST'])
 def upload_apk():
-
-    target = os.path.join(APP_ROOT, 'ZIP')
+    target = os.path.join(APP_ROOT, 'apk_to_img_files')
     print(target)
 
-    # if folder ZIP doesn't exist it will be created
+    # if folder apk_to_img_files doesn't exist it will be created
     if not os.path.isdir(target):
         os.mkdir(target)
-    print("'ZIP' folder has been created")
+    print("'apk_to_img_files' folder has been created")
+
+    # creation of a new folder where apk files will be stored
+    apkStorage = os.path.join(APP_ROOT, 'apk_to_img_files/APK_STORAGE')
+
+    # if folder APK_STORAGE doesn't exist it will be created
+    if not os.path.isdir(apkStorage):
+        os.mkdir(apkStorage)
+    print("'APK_STORAGE' folder has been created")
 
     # going to store file previously uploaded into ZIP folder
+    # going to store file previously uploaded into APK_STORAGE folder
+
     for file in request.files.getlist('file'):
         # changing the extension of the file previously from APK to ZIP
+
         fileName = 'apk-to-convert.zip'
         destination = "\\".join([target, fileName])
         print(destination)
         file.save(destination)
+
     print("File has been stored inside the folder")
 
-    # going to unzip file previously uploaded in 'unzipped_files' folder
-    filePath = 'ZIP/apk-to-convert.zip'
+    # copy app into APK_STORAGE changing the extension from .zip to .apk
 
-    unZipFolder = target + '/unzipped_files'
+    filePath = os.path.join(target, 'apk-to-convert.zip')
+    newPath = shutil.copy(filePath, apkStorage)
+    new_file_name = apkStorage + '/apk-to-convert.apk'
+    os.rename(newPath, new_file_name)
+
+    # hash code of APK to avoid storing duplicate application with a different name
+
+    hashName = hashlib.md5(open(new_file_name, 'rb').read()).hexdigest()
+
+    new_file_hash_name = apkStorage + '/' + hashName + '.apk'
+
+    # check if APK already exists in APK_STORAGE. If yes it will be removed
+    # else it will be renamed with the hash code generated
+
+    if not os.path.isfile(new_file_hash_name):
+        os.rename(new_file_name, new_file_hash_name)
+    else:
+        os.remove(new_file_name)
+        print('File already exists')
+
+    # going to unzip file previously uploaded in 'unzipped_files' folder
+    unZipFolder = os.path.join(target, 'unzipped_files')
 
     # if folder unzipped_files doesn't exist it will be created
     if not os.path.isdir(unZipFolder):
@@ -72,15 +109,14 @@ def upload_apk():
 
     # opens binary file old dex file and copy it in the file previously created
 
-    copyfile('ZIP/unzipped_files/classes.dex', 'binFile.txt')
+    source = os.path.join(target, 'unzipped_files/classes.dex')
 
+    copyfile(source, 'binFile.txt')
 
     byteFile = open('binFile.txt', 'rb').read()
 
     # conversion from binary to hexadecimal
     hex_data = codecs.encode(byteFile, "hex_codec")
-
-    print(hex_data)
 
     # deleting leftovers of byte file
 
@@ -88,13 +124,10 @@ def upload_apk():
     hex_data = (hex_data.replace("b'", ""))
     hex_data = (hex_data.replace("'", ""))
 
-    print(hex_data)
-
     # splitting the previous list in n list of length 6 characters
 
     long_list = [hex_data[i:i + 6] for i in range(0, len(hex_data), 6)]
     long_list.pop()
-    print(long_list)
 
     # creation of a new list where RGB code will be stored of each sublist previously created
 
@@ -104,12 +137,6 @@ def upload_apk():
 
     for element in long_list:
         colorList.append(tuple(int(element[i:i + 2], 16) for i in (0, 2, 4)))
-
-    print(colorList)
-
-    print(colorList)
-
-    print(len(colorList))
 
     # creation of a new image of dimension sqrt(len(colorList)
 
@@ -126,38 +153,44 @@ def upload_apk():
         for y in range(img_size):
             pixels[x, y] = colorList[img_size * x + y]
 
-    img.save("new-malware-img.jpg")
+    # creation of a new directory where images will be saved
+
+    imgFolder = os.path.join(target, 'images')
+
+    # if folder images doesn't exist it will be created
+    if not os.path.isdir(imgFolder):
+        os.mkdir(imgFolder)
+
+    img.save(imgFolder + "/new-malware-img.jpg")
 
     # resizing of the image previously created
 
-    img = Image.open('new-malware-img.jpg')
+    img = Image.open(imgFolder + '/new-malware-img.jpg')
 
     # rotate img of 90 degrees
 
     degrees = -90
     new_img = img.rotate(degrees, expand=True)
     new_img = new_img.resize((300, 300))
-    new_img.save("new-malware-img-300-300.jpg", "JPEG", optimize=True)
+    new_img.save(imgFolder + "/new-malware-img-300-300.jpg", "JPEG", optimize=True)
 
     # grayscale image conversion
 
-    grayScaleImg = Image.open('new-malware-img-300-300.jpg').convert('LA')
-    grayScaleImg.save('new-malware-img-300-300-greyscale.png')
+    grayScaleImg = Image.open(imgFolder + '/new-malware-img-300-300.jpg').convert('LA')
+    grayScaleImg.save(imgFolder + '/new-malware-img-300-300-greyscale.png')
 
     # elimination of elements that are no longer useful
 
     os.remove('binFile.txt')
     print('binFile has been removed \n')
-    os.remove('new-malware-img.jpg')
+    os.remove(imgFolder + '/new-malware-img.jpg')
     print('new-malware-img has been removed \n')
-    shutil.rmtree('ZIP', ignore_errors=True)
-    print('ZIP folder has been removed \n')
-
+    shutil.rmtree(unZipFolder, ignore_errors=True)
+    print('unzipped_files folder has been removed \n')
+    os.remove(filePath)
+    print('apk-to-convert.zip has been removed \n')
 
     return render_template('index.html')
-
-
-
 
 
 if __name__ == '__main__':
--- env/Lib/site-packages/cv2/version.py
+++
@@ -1,4 +0,0 @@
-opencv_version = "4.5.1.48"
-contrib = False
-headless = False
-ci_build = True
